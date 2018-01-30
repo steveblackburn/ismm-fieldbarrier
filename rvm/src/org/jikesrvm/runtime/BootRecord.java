57889ec73a2ab5cf49034476fda5f69821c0a17e
Steve Blackburn <steve.blackburn@anu.edu.au>
2018-01-30 20:28:10 +1100

    Remove whitespace

--- rvm/src/org/jikesrvm/runtime/BootRecord.java
+++ rvm/src/org/jikesrvm/runtime/BootRecord.java
@@ -169,14 +169,7 @@ public class BootRecord {
    * Print messages when delivering hardware exceptions to threads?
    * Set by -X:verboseSignalHandling
    */
-  public
-
-
-
-
-
-
-  int verboseSignalHandling = 0;
+  public int verboseSignalHandling = 0;
 
   // RVM startoff
   //
